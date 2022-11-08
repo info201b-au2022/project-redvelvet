29829a133f665024c938f5d54f25c02e841aef48
MOLLY BANKS <mbanks2@uw.edu>
2022-11-08 13:38:59 -0800

    load raw data

--- source/chart3.R
+++ source/chart3.R
@@ -1,1 +1,2 @@
-
+# load raw data from CSV in data folder
+salaries_raw <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-redvelvet/main/data/salary.csv", stringsAsFactors = FALSE)
