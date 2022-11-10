1c5a2dc29dfca10705d183036331b2fe80ab91b7
MOLLY BANKS <mbanks2@uw.edu>
2022-11-09 21:22:00 -0800

    refine plot 3

--- source/chart3.R
+++ source/chart3.R
@@ -15,9 +15,14 @@ salaries_edu <- salaries_raw %>%
 # Renders plot of salary by level of education
 job_edu_plot <- ggplot(data = salaries_edu) +
   geom_violin(mapping = aes(x = salary, y = Level.of.Education, fill = salary)) +
-  scale_fill_manual(values = c("#49A4B9", "#2C5985")) +
   scale_x_discrete("Annual Salary", labels=c('Less than 50k', 'Greater than 50k')) +
-  labs(title = "Education Impacts on Career Outcomes")
+  scale_y_continuous(name = "Level of Education", 
+                     breaks = c(1, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16), 
+                     labels = c("Pre-school", "Middle School","Some High School", "High School Diploma", "Some College", "Vocational", "Associates", "Bachelors", "Masters","Professional","Doctorate")) +
+  labs(title = "Education Impacts on Career Outcomes") +
+  scale_fill_manual(values = c("#49A4B9", "#2C5985")) +
+  theme(legend.position = "none")
+
 
 
   
