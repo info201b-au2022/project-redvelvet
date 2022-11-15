c1e140913845630d333ed28bb6b588278efc8a21
chunt170 <chunt170@uw.edu>
2022-11-14 17:36:14 -0800

    updates

--- source/summary.R
+++ source/summary.R
@@ -41,34 +41,3 @@ get_min_education <- function() {
     pull(education)
   return(summary_info$min_education[1])
 }
-
-
-
-# Create aggregated table of data
-get_table <- function() {
-  new_table <- salaries_raw %>%
-    group_by(salary) %>%
-    summarize(
-      HS.Diploma.Or.Less = sum(education == " Preschool" |
-        education == " 1st-4th" |
-        education == " 5th-6th" |
-        education == " 7th-8th" |
-        education == " 9th" |
-        education == " 10th" |
-        education == " 11th" |
-        education == " 12th" |
-        education == " HS-grad"),
-      College.Or.More = sum(education == " Some-college" |
-        education == " Assoc-acdm" |
-        education == " Assoc-voc" |
-        education == " Bachelors" |
-        education == " Masters" |
-        education == " Doctorate"),
-      age.17_39 = sum(age >= 20 & age <= 39),
-      age.40_90 = sum(age >= 40 & age <= 90),
-      us.origin = sum(native.country == " United-States"),
-      foreign.origin = sum(native.country != " United-States")
-    ) %>%
-    arrange(desc(salary))
-  return(new_table)
-}
