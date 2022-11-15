d5168ddcd9d9616f5100d269fd004eac5f2bc3f1
manavunown11 <manavag@uw.edu>
2022-11-14 18:34:23 -0800

    done

--- source/chart1.R
+++ source/chart1.R
@@ -10,8 +10,10 @@ salaries_age <- salaries %>% select(age, salary)
 age_graph <- ggplot(data = salaries_age) +
   geom_bar(aes(x = salary, fill = age)) +
   scale_x_discrete("Annual Salary", labels = c("Less than 50k", "Greater than 50k")) +
-  scale_y_continuous(name = "Age Vs Salary")
+  scale_y_continuous(name = "Age Vs Salary", breaks = c(5000, 10000, 15000, 20000, 25000),
+                     labels = c("20", "30", "40", "50", "60"))
 
 plot_age <- function(){
   plot(age_graph)
 }
+plot_age()
