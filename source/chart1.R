26d71fb05cfa4824f5cd21600efe04f25326d8fa
manavunown11 <manavag@uw.edu>
2022-11-14 16:38:38 -0800

    manav graph

---
+++ source/chart1.R
@@ -0,0 +1,15 @@
+# loading packages
+library(ggplot2)
+library(dplyr)
+
+# loading data from CSV in data folder
+salaries <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-redvelvet/main/data/salary.csv", stringsAsFactors = FALSE)
+
+salaries_age <- salaries %>% select(age, salary)
+
+age_graph <- ggplot(data = salaries_age) +
+  geom_bar(aes(x = salary, fill = age)) +
+  scale_x_discrete("Annual Salary", labels = c("Less than 50k", "Greater than 50k")) +
+  scale_y_continuous(name = "Age Vs Salary")
+
+age_graph
