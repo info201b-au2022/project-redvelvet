a19fb5192ed9f87579e2cf54fd8aa6ff501b7418
MOLLY BANKS <mbanks2@uw.edu>
2022-11-30 17:39:54 -0800

    ggplotly

--- source/chart3.R
+++ source/chart3.R
@@ -5,41 +5,50 @@ library(dplyr)
 # load raw data from CSV in data folder
 salaries_raw <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-redvelvet/main/data/salary.csv", stringsAsFactors = FALSE)
 
+write.csv(salaries_raw, file = "salary.csv")
+
+
 # Renders data frame with relevant features and new names for clarity
-salaries_edu <- salaries_raw %>%
-  rename(Level.of.Education = education.num) %>%
-  select(Level.of.Education, salary, education, sex)
 
-# Renders plot of salary by level of education
-edu_plot <- ggplot(data = salaries_edu) +
-  geom_violin(mapping = aes(
-    x = salary,
-    y = Level.of.Education,
-    fill = salary
-  )) +
-  scale_x_discrete("Annual Salary",
-    labels = c("Less than 50k", "Greater than 50k")
-  ) +
-  scale_y_continuous(
-    name = "Level of Education",
-    breaks = c(1, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16),
-    labels = c(
-      "Pre-school", "Middle School", "Some High School",
-      "High School Diploma", "Some College", "Vocational",
-      "Associates", "Bachelors", "Masters",
-      "Professional", "Doctorate"
-    )
-  ) +
-  labs(title = "Education Impacts on Salary Outcomes") +
-  scale_fill_manual(values = c("#49A4B9", "#2C5985")) +
-  theme(legend.position = "none")
-
-# function to render plot
-plot_edu <- function() {
-  plot(edu_plot)
+get_df <- function() {
+  salaries_edu <- salaries_raw %>%
+    rename(Level.of.Education = education.num) %>%
+    select(Level.of.Education, salary, education, sex, age)
+  
+  return(salaries_edu)
 }
 
+# Renders plot of salary by level of education
 
-# saves plot as png for output file
+# function to render plot
+plotly_edu <- function() {
+  salaries_edu <- get_df()
+  
+  org_plot <- ggplot(data = salaries_edu) +
+    geom_violin(mapping = aes(
+      x = salary,
+      y = Level.of.Education,
+      fill = salary
+    )) +
+    scale_x_discrete("Annual Salary",
+                     labels = c("Less than 50k", "Greater than 50k")
+    ) +
+    scale_y_continuous(
+      name = "Level of Education",
+      breaks = c(1, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+      labels = c(
+        "Pre-school", "Middle School", "Some High School",
+        "High School Diploma", "Some College", "Vocational",
+        "Associates", "Bachelors", "Masters",
+        "Professional", "Doctorate"
+      )
+    ) +
+    labs(title = "Education Impacts on Salary Outcomes") +
+    scale_fill_manual(values = c("#49A4B9", "#2C5985")) +
+    theme(legend.position = "none")
+  
+  edu_plot <- ggplotly(org_plot)
+  
+  return(edu_plot)
+}
 
-ggsave("edu_salary_chart3.png")
